ce207731b7ea522212112bff21ac2c0060b7fe0e
panheng1971 <pan.heng@hotmail.com>
2021-03-25 17:16:41 +0800

    Add project files.

---
+++ Camera/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Camera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.comboBoxSelectCam = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.start.Location = new System.Drawing.Point(132, 385);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(324, 385);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(101, 59);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(375, 265);
+            this.videoSourcePlayer1.TabIndex = 2;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // comboBoxSelectCam
+            // 
+            this.comboBoxSelectCam.FormattingEnabled = true;
+            this.comboBoxSelectCam.Location = new System.Drawing.Point(101, 12);
+            this.comboBoxSelectCam.Name = "comboBoxSelectCam";
+            this.comboBoxSelectCam.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxSelectCam.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxSelectCam);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.ComboBox comboBoxSelectCam;
+    }
+}
+
---
+++ Camera/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+namespace Camera
+{
+  
+    public partial class Form1 : Form
+    {
+
+        private FilterInfoCollection videoDevices;//所有摄像设备
+        private VideoCaptureDevice videoDevice;//摄像设备
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);//得到所有接入的摄像设备
+            if (videoDevices.Count != 0)
+            {
+                foreach (FilterInfo device in videoDevices)
+                {
+                    comboBoxSelectCam.Items.Add(device.Name);//把摄像设备添加到摄像列表中                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("没有找到摄像头！");
+            }
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            videoDevice = new VideoCaptureDevice(videoDevices[comboBoxSelectCam.SelectedIndex].MonikerString);
+            videoSourcePlayer1.VideoSource = videoDevice;
+            videoSourcePlayer1.SignalToStop();
+            videoSourcePlayer1.WaitForStop();
+            videoSourcePlayer1.Start();
+        }
+
+        private void videoSourcePlayer1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            videoSourcePlayer1.Stop();
+        }
+    }
+}
